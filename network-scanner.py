bad12da64c57fae66cf5aaf5ca4e4b0a29ed2b9c
nelson <nelson.acosta.web@gmail.com>
2019-05-31 22:46:12 -0400

    version 2.0

--- network-scanner.py
+++ network-scanner.py
@@ -1,6 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import scapy.all as scapy
+import argparser
+
+
+def getArguments():
+    parser = argparser.ArgumentParser()
+    parser.add_argument("-t", "--target", dest=target,
+                        help="Target IP / IP range.")
+    options = parser.parse_args()
+    return options
 
 
 def scan(ip):
@@ -9,9 +18,20 @@ def scan(ip):
     arp_requets_broadcast = broadcast/arp_requets
     answered_list = scapy.srp(arp_requets_broadcast,
                               timeout=1, verbose=False)[0]
-    print("IP\t\t\tMAC Address\n--------------------------------------------------------------")
+    clients_list = []
     for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
         print(element[1].psrc + "\t\t" + element[1].hwsrc)
+    return clients_list
 
 
-scan("192.168.0.1/24")
+def print_relsult(result_list):
+    print("IP\t\t\tMAC Address\n--------------------------------------------------------------")
+    for client in result_list:
+        print(client["ip" + "\t\t" + client["mac"]])
+
+
+options = get_arguments()
+scan_result = scan(options.target)
+print_relsult(scan_result)
