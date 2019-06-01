34d216720813ba30b080c27f45b9c2785263ee0f
nelson <nelson.acosta.web@gmail.com>
2019-05-31 21:18:08 -0400

    version 1.0

--- network-scanner.py
+++ network-scanner.py
@@ -4,7 +4,14 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_requets = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_requets_broadcast = broadcast/arp_requets
+    answered_list = scapy.srp(arp_requets_broadcast,
+                              timeout=1, verbose=False)[0]
+    print("IP\t\t\tMAC Address\n--------------------------------------------------------------")
+    for element in answered_list:
+        print(element[1].psrc + "\t\t" + element[1].hwsrc)
 
 
 scan("192.168.0.1/24")
